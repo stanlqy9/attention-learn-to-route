3f06335dec0ac978c2248c3b4c303468c588b90c
stanlqy9 <sn859627@gmail.com>
2026-05-31 16:27:31 -0400

    Deleted unused files, folders & code

--- utils/functions.py
+++ utils/functions.py
@@ -11,14 +11,9 @@ import torch.nn.functional as F
 
 
 def load_problem(name):
-    from problems import TSP, CVRP, SDVRP, OP, PCTSPDet, PCTSPStoch
+    from problems import OP
     problem = {
-        'tsp': TSP,
-        'cvrp': CVRP,
-        'sdvrp': SDVRP,
         'op': OP,
-        'pctsp_det': PCTSPDet,
-        'pctsp_stoch': PCTSPStoch,
     }.get(name, None)
     assert problem is not None, "Currently unsupported problem: {}!".format(name)
     return problem
